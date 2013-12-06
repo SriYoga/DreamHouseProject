2a6396461d0380a6f1dd8804e6884cc9f5342982
Helen(Pei)Wang Developer <pwang39@my.centennialcollege.ca>
2013-12-05 21:28:25 -0500

    testsuitesclass

---
+++ Source Code/UnitTests/DreamHouseTestSuites.java
@@ -0,0 +1,13 @@
+package comp231.DreamHouse.tests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import DreamHouse.AddNewHouse;
+
+@RunWith(Suite.class)
+@SuiteClasses ({LoginTest.class,AddNewHouse.class,	AddImageTest.class,	SearchResultTest.class,	UpdateHouseTest.class})
+
+public class DreamHouseTestSuites {
+	
+
+}
