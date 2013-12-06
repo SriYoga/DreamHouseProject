50a959ebc194fe58aa575d4966333f21e42f4ad2
Helen(Pei)Wang Developer <pwang39@my.centennialcollege.ca>
2013-12-06 00:42:35 -0500

    fix the bug in AddNewHouse.class

--- Source Code/AddNewHouse.java
+++ Source Code/AddNewHouse.java
@@ -1,5 +1,4 @@
-package dh;
-
+package DreamHouse;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Image;
@@ -27,6 +26,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Random;
+
 import javax.swing.border.LineBorder;
 import java.awt.Color;
 
@@ -45,7 +46,7 @@ public class AddNewHouse extends JFrame implements ActionListener {
 	public AddNewHouse(UserDetail user) {
 		setTitle("Home - Add new house");
 		// setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 585, 420);
+		setBounds(100, 100, 769, 579);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -53,124 +54,127 @@ public class AddNewHouse extends JFrame implements ActionListener {
 		contentPane.setLayout(null);
 
 		JLabel lblWelcome = new JLabel("Welcome");
-		lblWelcome.setBounds(36, 11, 67, 14);
+		lblWelcome.setBounds(165, 21, 67, 14);
 		contentPane.add(lblWelcome);
 
 		JLabel label = new JLabel(user.FirstName + " " + user.LastName);
-		label.setBounds(113, 11, 316, 14);
+		label.setBounds(242, 21, 316, 14);
 		contentPane.add(label);
 
 		JLabel lblLocation = new JLabel("Location");
-		lblLocation.setBounds(36, 51, 67, 14);
+		lblLocation.setBounds(195, 169, 67, 14);
 		contentPane.add(lblLocation);
 
 		JLabel lblHouseType = new JLabel("House Type");
-		lblHouseType.setBounds(36, 93, 87, 14);
+		lblHouseType.setBounds(195, 201, 87, 14);
 		contentPane.add(lblHouseType);
 
 		JLabel lblSquareFeet = new JLabel("Square Feet");
-		lblSquareFeet.setBounds(36, 134, 87, 14);
+		lblSquareFeet.setBounds(195, 238, 87, 14);
 		contentPane.add(lblSquareFeet);
 
 		JLabel lblPriceRange = new JLabel("Price Range");
-		lblPriceRange.setBounds(36, 178, 87, 14);
+		lblPriceRange.setBounds(195, 279, 87, 14);
 		contentPane.add(lblPriceRange);
 
 		JLabel lblDescription = new JLabel("Description:");
-		lblDescription.setBounds(36, 219, 102, 14);
+		lblDescription.setBounds(192, 341, 102, 14);
 		contentPane.add(lblDescription);
 
 		final JTextArea txtDescription = new JTextArea();
 		txtDescription.setBorder(new LineBorder(new Color(0, 0, 0)));
 		txtDescription.setWrapStyleWord(true);
 		txtDescription.setLineWrap(true);
-		txtDescription.setBounds(36, 244, 227, 77);
+		txtDescription.setBounds(192, 366, 250, 84);
 		contentPane.add(txtDescription);
 
-		String[] locationStrings = { "Toronto","Missisauga", "Ottawa", "Hamilton",
+		String[] locationStrings = { "Toronto", "Ottawa", "Hamilton",
 				"Kingston", "Oshawa", "Montreal" };
 		final JComboBox cmbLocation = new JComboBox(locationStrings);
-		cmbLocation.setBounds(133, 51, 149, 20);
+		cmbLocation.setBounds(280, 166, 149, 20);
 		contentPane.add(cmbLocation);
 
-		String[] typeStrings = { "Appartment", "Condominiums",
+		String[] typeStrings = { "Apartment", "Condominiums",
 				"Detached House", "Semi-detached House", "Townhouse",
 				"Duplex/Triplex" };
 		final JComboBox cmbType = new JComboBox(typeStrings);
-		cmbType.setBounds(133, 93, 150, 20);
+		cmbType.setBounds(280, 199, 150, 20);
 		contentPane.add(cmbType);
 
 		final JTextField txtSqFeet = new JTextField();
-		txtSqFeet.setBounds(133, 134, 150, 20);
+		txtSqFeet.setBounds(279, 238, 150, 20);
 		contentPane.add(txtSqFeet);
 
 		final JTextField txtPrice = new JTextField();
-		txtPrice.setBounds(133, 178, 150, 20);
+		txtPrice.setBounds(278, 277, 150, 20);
 		contentPane.add(txtPrice);
 
 		JLabel lblBedRooms = new JLabel("Bed Rooms");
-		lblBedRooms.setBounds(313, 51, 77, 14);
+		lblBedRooms.setBounds(481, 169, 77, 14);
 		contentPane.add(lblBedRooms);
 
 		JLabel lblWashRooms = new JLabel("Wash Rooms");
-		lblWashRooms.setBounds(313, 93, 77, 14);
+		lblWashRooms.setBounds(481, 212, 77, 14);
 		contentPane.add(lblWashRooms);
 
 		JLabel lblGarage = new JLabel("Garage");
-		lblGarage.setBounds(313, 134, 46, 14);
+		lblGarage.setBounds(481, 248, 46, 14);
 		contentPane.add(lblGarage);
 
 		JLabel lblSelectPictureTo = new JLabel("Upload picture");
-		lblSelectPictureTo.setBounds(313, 178, 87, 14);
+		lblSelectPictureTo.setBounds(481, 283, 87, 14);
 		contentPane.add(lblSelectPictureTo);
 
 		final JTextField txtBedRooms = new JTextField();
-		txtBedRooms.setBounds(401, 48, 128, 20);
+		txtBedRooms.setBounds(569, 166, 128, 20);
 		contentPane.add(txtBedRooms);
 
 		final JTextField txtWashRooms = new JTextField();
-		txtWashRooms.setBounds(400, 90, 129, 20);
+		txtWashRooms.setBounds(568, 209, 129, 20);
 		contentPane.add(txtWashRooms);
 
 		final JTextField txtGarage = new JTextField();
-		txtGarage.setBounds(401, 131, 128, 20);
+		txtGarage.setBounds(569, 245, 128, 20);
 		contentPane.add(txtGarage);
 
 		fc = new JFileChooser();
 
 		btnUpload = new JButton("Upload");
 		btnUpload.addActionListener(this);
-		btnUpload.setBounds(401, 174, 128, 23);
+		btnUpload.setBounds(569, 279, 128, 23);
 		contentPane.add(btnUpload);
 
 		lblImage = new JLabel("");
 		lblImage.setBorder(new LineBorder(new Color(0, 0, 0)));
-		lblImage.setBounds(313, 219, 232, 102);
+		lblImage.setBounds(475, 355, 232, 102);
 		contentPane.add(lblImage);
 
 		JButton btnInsert = new JButton("Insert");
 		btnInsert.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
+					
+					Random rd=new Random();
+					int homeId=rd.nextInt();
 					connection = DriverManager.getConnection(
-							"jdbc:oracle:thin:@localhost:1521:orcl", "sysman",
+							"jdbc:oracle:thin:@localhost:1521:VTM", "sys as sysdba",
 							"sysman123");
 					PreparedStatement ps = connection
-							.prepareStatement("insert into Home(location,type,sqfeet,price,description,bedrooms,washrooms,garage,image)"
-									+ "values(?,?,?,?,?,?,?,?,?)");
+							.prepareStatement("insert into Home(homeid,location,type,sqfeet,price,description,bedrooms,washrooms,garage,image)"
+									+ "values(?,?,?,?,?,?,?,?,?,?)");
 
-					FileInputStream fin = new FileInputStream(fc
-							.getSelectedFile().getPath());
+					FileInputStream fin = new FileInputStream(fc.getSelectedFile().getPath());
 					//System.out.println(fc.getSelectedFile().getPath());
-					ps.setString(1, cmbLocation.getSelectedItem().toString());
-					ps.setString(2, cmbType.getSelectedItem().toString());
-					ps.setString(3, txtSqFeet.getText().toString());
-					ps.setString(4, txtPrice.getText().toString());
-					ps.setString(5, txtDescription.getText());
-					ps.setString(6, txtBedRooms.getText().toString());
-					ps.setString(7, txtWashRooms.getText().toString());
-					ps.setString(8, txtGarage.getText().toString());
-					ps.setBinaryStream(9, fin, fin.available());
+					ps.setLong(1,homeId);
+					ps.setString(2, cmbLocation.getSelectedItem().toString());
+					ps.setString(3, cmbType.getSelectedItem().toString());
+					ps.setString(4, txtSqFeet.getText().toString());
+					ps.setString(5, txtPrice.getText().toString());
+					ps.setString(6, txtDescription.getText());
+					ps.setString(7, txtBedRooms.getText().toString());
+					ps.setString(8, txtWashRooms.getText().toString());
+					ps.setString(9, txtGarage.getText().toString());
+					ps.setBinaryStream(10, fin, fin.available());
 					ps.executeUpdate();
 					JOptionPane.showMessageDialog(null,
 							"Home details successfully inserted",
@@ -181,18 +185,23 @@ public class AddNewHouse extends JFrame implements ActionListener {
 				}
 			}
 		});
-		btnInsert.setBounds(344, 347, 89, 23);
+		btnInsert.setBounds(496, 489, 89, 23);
 		contentPane.add(btnInsert);
 
 		btnClose = new JButton("Close");
 		btnClose.addActionListener(this);
-		btnClose.setBounds(456, 347, 89, 23);
+		btnClose.setBounds(608, 489, 89, 23);
 		contentPane.add(btnClose);
 
 		btnLogout = new JButton("Logout");
 		btnLogout.addActionListener(this);
-		btnLogout.setBounds(456, 7, 89, 23);
+		btnLogout.setBounds(618, 31, 89, 23);
 		contentPane.add(btnLogout);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon(AddNewHouse.class.getResource("/Resources/AddNewHouse1.jpg")));
+		label_1.setBounds(0, 0, 753, 540);
+		contentPane.add(label_1);
 	}
 
 	@Override
